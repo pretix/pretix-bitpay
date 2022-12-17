b53da686fc26a16e88f3ba18a5846fbae483bef6
Raphael Michel <michel@rami.io>
2022-12-17 16:45:59 +0100

    Stop using deprecated Django APIs

--- pretix_bitpay/urls.py
+++ pretix_bitpay/urls.py
@@ -1,18 +1,18 @@
-from django.conf.urls import url, include
+from django.urls import include, path, re_path
 
 from pretix.multidomain import event_url
 from .views import auth_start, auth_disconnect, webhook, ReturnView, redirect_view
 
 event_patterns = [
-    url(r'^bitpay/', include([
+    path('bitpay/', include([
         event_url(r'^webhook/$', webhook, name='webhook', require_live=False),
         event_url(r'^redirect/$', redirect_view, name='redirect', require_live=False),
-        url(r'^return/(?P<order>[^/]+)/(?P<hash>[^/]+)/(?P<payment>[^/]+)/$', ReturnView.as_view(), name='return'),
+        path('return/<str:order>/<str:hash>/<str:payment>/', ReturnView.as_view(), name='return'),
     ])),
 ]
 urlpatterns = [
-    url(r'^control/event/(?P<organizer>[^/]+)/(?P<event>[^/]+)/bitpay/connect/',
-        auth_start, name='auth.start'),
-    url(r'^control/event/(?P<organizer>[^/]+)/(?P<event>[^/]+)/bitpay/disconnect/',
-        auth_disconnect, name='auth.disconnect'),
+    re_path(r'^control/event/(?P<organizer>[^/]+)/(?P<event>[^/]+)/bitpay/connect/',
+            auth_start, name='auth.start'),
+    re_path(r'^control/event/(?P<organizer>[^/]+)/(?P<event>[^/]+)/bitpay/disconnect/',
+            auth_disconnect, name='auth.disconnect'),
 ]
