96fcf7c326e9832eef51d0f1e3a11d8377dc5521
Raphael Michel <michel@rami.io>
2022-12-19 10:26:42 +0100

    Add default=True to AppConfig to ensure backwards compatibility

---
+++ pretix_bitpay/apps.py
@@ -0,0 +1,32 @@
+from django.apps import AppConfig
+from django.utils.functional import cached_property
+from django.utils.translation import gettext_lazy as _
+
+from . import __version__
+
+
+class BitpayApp(AppConfig):
+    name = 'pretix_bitpay'
+    verbose_name = _("BitPay")
+
+    class PretixPluginMeta:
+        name = _("BitPay")
+        author = "Raphael Michel"
+        category = 'PAYMENT'
+        version = __version__
+        description = _("Receive Crypto payments via BitPay-compatible payment providers.")
+        picture = "pretix_bitpay/bitpay-logo.svg"
+
+    def ready(self):
+        from . import signals  # NOQA
+
+    @cached_property
+    def compatibility_errors(self):
+        errs = []
+        try:
+            import btcpay  # NOQA
+        except ImportError:
+            errs.append("Python package 'btcpay' is not installed.")
+        return errs
+
+
